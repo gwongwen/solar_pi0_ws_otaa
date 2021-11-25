cc8ebe1c229f4711b3801ad07a0cb734908db252
gwongwen <gwongwen.trash@gmail.com>
2021-11-25 08:26:42 +0100

    project updated and cleaned

--- 01-hardware/tests/test_bmp3xx.py
+++ 01-hardware/tests/test_bmp3xx.py
@@ -1,13 +1,14 @@
-
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
+#! /usr/bin/env python3
+# test for bmp388 weather sensor 
+# version 1.0 - 19/11/21
+# version 1.1 - 11/25/21 (change while loop to for loop with 5 measures)
 
 import time
 import board
 import adafruit_bmp3xx
 
 # create library object using our Bus I2C port
-i2c = board.I2C()  # uses board.SCL and board.SDA
+i2c = board.I2C()                           # uses board.SCL and board.SDA
 bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
 
 # change this to match the location's pressure (hPa) at sea level
@@ -21,9 +22,10 @@ bmp.temperature_oversampling = 1
 # separate temperature sensor to calibrate this one.
 temperature_offset = -5
 
-while True:
-    print("\nTemperature: %0.1f C" % (bmp.temperature + temperature_offset))
+for meas in range (0,5,1):
+    print("Temperature: %0.1f C" % (bmp.temperature + temperature_offset))
     print("Pressure: %0.3f hPa" % bmp.pressure)
     print("Altitude = %0.2f meters" % bmp.altitude)
 
     time.sleep(1)
+
