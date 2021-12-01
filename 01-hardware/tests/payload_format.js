6418185111022ed67e669c8b7ae8f2392d929ce2
gwongwen <gwongwen.trash@gmail.com>
2021-12-01 08:20:31 +0100

    simplify decoder code

--- 01-hardware/tests/payload_format.js
+++ 01-hardware/tests/payload_format.js
@@ -1,12 +1,11 @@
 function Decoder(bytes, port) {
     var decoded = {};
-    decoded.Pressure = ((bytes[0] << 16) | (bytes[1] << 8) | bytes[2])/100
-    if(bytes[3] == 0){
-        decoded.Temperature = ((bytes[4] << 8) | bytes[5])/100
+    decoded.Pressure = ((bytes[0] << 8) | bytes[1])/100
+    if(bytes[2] === 0){
+        decoded.Temperature = ((bytes[3] << 8) | bytes[4])/100
     }else{
-        decoded.Temperature = (((bytes[4] << 8)| bytes[5]) * -1)/100
+        decoded.Temperature = (((bytes[3] << 8)| bytes[4]) * -1)/100
     }
-    decoded.Altitude = ((bytes[11] << 16) | (bytes[12] << 8) | bytes[13])/100
-
+ 
     return decoded;
   }
