613726dfa2ec12220cf6744d4f72dca3507bc652
gwongwen <gwongwen.trash@gmail.com>
2021-12-13 15:11:14 +0100

    bug fixes and simplification

--- 01-hardware/tests/test_send_lora_otaa.py
+++ 01-hardware/tests/test_send_lora_otaa.py
@@ -1,26 +1,47 @@
 #! /usr/bin/env python3
-# test: send physical data from sensor to LoRaWAN TTN by OTAA identification
+# test: send join request from sensor to LoRaWAN TTN by OTAA identification
 # version 1.0 - 23/11/21
 
 import time, board, LoRaWAN
 from time import sleep
 from SX127x.LoRa import *
-from SX127x.LoRaArgumentParser import LoRaArgumentParser
 from SX127x.board_config import BOARD
 from LoRaWAN.MHDR import MHDR
 from random import randrange
 
 BOARD.setup()
-parser = LoRaArgumentParser("LoRaWAN sender")
 
 class LoRaWANotaa(LoRa):
-    def __init__(self, verbose = False):
+    def __init__(self, verbose = True):
         super(LoRaWANotaa, self).__init__(verbose)
     
     def on_tx_done(self):
         self.set_mode(MODE.STDBY)
         self.clear_irq_flags(TxDone=1)
+        self.set_dio_mapping([0,0,0,0,0,0])
+        self.set_invert_iq(1)
+        self.reset_ptr_rx()
+        self.set_mode(MODE.RXCONT)
         print("TxDone")
+
+    def on_rx_done(self):
+        self.clear_irq_flags(RxDone=1)
+        payload = self.read_payload(nocheck=True)
+        print("RxDone")
+
+        lorawan = LoRaWAN.new([], appkey)
+        lorawan.read(payload)
+        print(lorawan.get_payload())
+        print(lorawan.get_mhdr().get_mversion())
+
+        if lorawan.get_mhdr().get_mtype() == MHDR.JOIN_ACCEPT:
+            print("Got LoRaWAN join accept")
+            print(lorawan.valid_mic())
+            print(lorawan.get_devaddr())
+            print(lorawan.derive_nwskey(devnonce))
+            print(lorawan.derive_appskey(devnonce))
+            print("\n")
+            sys.exit(0)
 
     def start(self):
         self.tx_counter = 1
@@ -36,7 +57,7 @@ deveui = [0x00, 0x47, 0x64, 0xB1, 0xAB, 0xC6, 0x4F, 0x7C]
 appeui = [0x70, 0xB3, 0xD5, 0x7E, 0xF0, 0x00, 0x51, 0x34]
 appkey = [ 0x4A, 0xD7, 0xB6, 0x3F, 0x86, 0xAB, 0xC7, 0x54, 0xCF, 0x26, 0x8E, 0xE5, 0x60, 0xDE, 0x1C, 0x99]
 devnonce = [randrange(256), randrange(256)]
-lora = LoRaWANotaa(False)
+lora = LoRaWANotaa(True)
 
 # setup
 lora.set_mode(MODE.SLEEP)
